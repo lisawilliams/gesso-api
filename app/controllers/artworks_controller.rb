565e0c0aee888523e7f1aa184f0e9462be96e5ee
lisawilliams <lisa@placeblogger.com>
2017-07-20 14:21:04 -0400

    Wrote `create` function in artworks_controller
    Added code to allow us to associate an artwork with a show.
    Added curl script to create an artwork and tested curl script.
    
    Script returns `201 Created` as expected.
    Confirmed that artwork created is in the `artworks` table in the
    database with the correct values and correct associated show.
    
    Closes Issue #36.
    
    Closes Capstone Issue 771.
    https://github.com/ga-wdi-boston/capstone-project/issues/771

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -13,13 +13,20 @@ class ArtworksController < OpenReadController
     render json: @artwork
   end
 
-  # create @show variable to allow the POST artworks to reference a particular
-  # show via show_id
 
   # POST /artworks
   def create
+    # captures params from artwork object; show_id is a property of 'artwork'
     @show_id = params[:artwork][:show_id]
-    @artwork = current_user.shows.artwork.build(artwork_params)
+    # now that we have grabbed the show_id from the artwork object
+    # we can use it to define a variable called `current show`
+    # note: it's important to use @ before show_id on line 23 and before current_show on line 29
+    @current_show = current_user.shows.find(@show_id)
+    # @artworks now uses current_show to build an artwork
+    # from current_show with the params from the artwork object
+    # to get a better idea of the artwork object look at db/schema.rb
+    # or /scripts/create-artwork.sh
+    @artwork = @current_show.artworks.build(artwork_params)
     if @artwork.save
       render json: @artwork, status: :created, location: @artwork
     else
